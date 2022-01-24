f7ffd24d05ea2e091c8827d04d8f775866b90f6b
b4stian <bastian@ajikeshi.com>
2022-01-24 23:41:42 +0100

    Added poetry, set up file structure

---
+++ anabu/analysis.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# import skimage
+
+
+def run():
+    """Runs the whole analysis script."""
+    print("Analysis run!")
+
+
+if __name__ == "__main__":
+    run()
---
+++ anabu/mask.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+
+class mask:
+    """Object defining the masking of a photo file for analysis."""
+
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def print():
+        print("!!!")
