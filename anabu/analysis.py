8f1c1fcb5c5eb65efb8ad8291ae23a05ce636f5c
Bastian Ebeling <mail@bastianebeling.org>
2022-01-25 00:49:25 +0100

    first logging messages

--- anabu/analysis.py
+++ anabu/analysis.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-# import skimage
+import interface
 
+version = "0.2.0"
 
 def run():
     """Runs the whole analysis script."""
-    print("Analysis run!")
+    interface.logging.info("--------------------------------------------------------")
+    interface.logging.info(f"Process started. Ink homogeneity evaluator version {str(version)}.")
+    interface.logging.info("--------------------------------------------------------")
+    interface.logging.info("In case of problems with this tool contact bastian.ebeling@kuraray.com.")
+    interface.logging.info("Imports successful.")
 
 
 if __name__ == "__main__":
