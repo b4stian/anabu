2cd369bb89c220f79d574956f5ee144c1efe7fec
b4stian <bastian@ajikeshi.com>
2022-02-03 14:30:22 +0100

    basic ppt export

--- anabu/ppt.py
+++ anabu/ppt.py
@@ -9,4 +9,10 @@ from pptx.enum.text import PP_ALIGN
 # ------------------------------------------------
 # executions
 
-prs = Presentation()
+prs = Presentation(r"ppt/PPT_master.pptx")
+
+title_slide = prs.slides.add_slide(prs.slide_layouts[0])
+title_slide.placeholders[0].text = "[Sample name]"
+title_slide.placeholders[13].text = "Evaluation results for light table photo"
+
+prs.save(r"test_photos/anabu_results.pptx")
